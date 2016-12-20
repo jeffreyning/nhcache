bdbdace2980c5a8e3f0c3bbd666528fe46ac44d7
ninghao <ninghao@minxinjituan.cn>
2016-12-20 16:22:33 +0800

    const

--- nhcache-base/src/main/java/com/nh/cache/base/NhCacheConst.java
+++ nhcache-base/src/main/java/com/nh/cache/base/NhCacheConst.java
@@ -1,7 +1,7 @@
 package com.nh.cache.base;
 
 public interface NhCacheConst {
-public static final String CACHE_VERSION="cache_version";
+public static final String CACHE_VERSION="nhcache_version";
 //public static final String CACHE_KEY="cache_key";
 public static final String CACHE_PREFIX="nhcache_";
 }
